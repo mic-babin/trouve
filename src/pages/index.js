f7d3669a15a46240df5926c549fc5033e8bd7be1
Michael Babin <info@micb.dev>
2022-12-30 09:10:37 -0500

    Maybe? who knows

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,11 @@ import * as sections from "../components/home/index-sections.component";
 import Fallback from "../components/fallback.component";
 // import { SEO } from "../components/seo";
 import { useState, useEffect, useRef } from "react";
-import Loader from "../components/common/loader.component";
-import FirstLoader from "../components/common/first-loader.component";
 
 export default function Homepage(props) {
   const path = props.path;
   const menu = props.data.allContentfulHeader.edges[0].node;
+
   const contact = props.data.allContentfulPage.edges[1].node.sections.filter(
     (section) =>
       section.id === "b8dc3482-8f6b-52e3-9c2a-cdf3971f3a76" ||
@@ -18,13 +17,8 @@ export default function Homepage(props) {
       section.id === "33167fe8-1da1-59ca-8cae-8aed5506436b" ||
       section.id === "6609d98c-4bf8-5936-9f03-9e293bbd3542"
   );
-
   const home = props.data.allContentfulPage.edges[0].node.sections;
-  const firstLoader = home.filter(
-    (section) =>
-      section.id === "0fa3a0bf-6404-5dd4-b9da-129729d5e326" ||
-      section.id === "92e9cacb-db9a-506c-8711-563d732976d5"
-  )[0];
+  console.log(home);
   const layout = useRef();
 
   const [showContact, setShowContact] = useState(false);
@@ -33,28 +27,15 @@ export default function Homepage(props) {
 
   const handleHeaderColor = (color) => setHeaderColor(color);
 
-  // let data =
-  //   typeof window !== "undefined" ? sessionStorage.getItem("firstLoader") : "";
-
   const [showPage, setShowPage] = useState(false);
-  // const [showFirstLoader, setShowFirstLoader] = useState(
-  //   data === "shown" ? false : true
-  // );
-  // const [showFirstLoader, setShowFirstLoader] = useState(false);
+  console.log(showPage);
   useEffect(() => {
-    // if (showFirstLoader) {
-    //   setTimeout(() => {
-    //     setShowFirstLoader(false);
-    //     // if (typeof window !== "undefined") {
-    //     //   sessionStorage.setItem("firstLoader", "shown");
-    //     // }
-    //   }, 2100);
-    // }
     if (!showPage) {
       setTimeout(() => {
         setShowPage(true);
       }, 1);
     }
+
     if (showPage) {
       var observer = new IntersectionObserver(
         function (entries) {
@@ -66,9 +47,9 @@ export default function Homepage(props) {
         },
         { threshold: [0.1] }
       );
-      if (document.querySelector("#hero")) {
+
+      if (document.querySelector("#hero"))
         observer.observe(document.querySelector("#hero"));
-      }
     }
 
     return () => {
@@ -87,10 +68,7 @@ export default function Homepage(props) {
         setShowContact={setShowContact}
         headerColor={headerColor}
         path={path}
-        showPage={showPage}
       >
-        {/* <FirstLoader image={firstLoader} show={showFirstLoader} /> */}
-        {!showPage && <Loader />}
         <div id="top"></div>
         {showPage &&
           home.map((section) => {
@@ -172,12 +150,6 @@ export const query = graphql`
         node {
           id
           sections {
-            ... on ContentfulLoader {
-              id
-              loaderImage {
-                gatsbyImageData(placeholder: BLURRED)
-              }
-            }
             ... on ContentfulSection {
               id
               type
