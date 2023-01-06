deb18cbb2e1ab721d2c447ae4b71bd14706d2cd2
Michael Babin <info@micb.dev>
2023-01-05 19:10:55 -0500

    More safari mumbo jumbo

--- src/components/home/experience.component.js
+++ src/components/home/experience.component.js
@@ -61,7 +61,7 @@ const Experience = ({ data }) => {
         <motion.div
           initial={{
             height: "0px",
-            left: isSafari ? "calc(16.66vw + 13px)" : "calc(16.66vw + 10px)",
+            left: isSafari ? "calc(16.66vw + 7px)" : "calc(16.66vw + 10px)",
           }}
           whileInView={{
             height: isSmall ? "180px" : isMedium ? "220px" : "500px",
@@ -77,7 +77,7 @@ const Experience = ({ data }) => {
         <motion.div
           initial={{
             height: "0px",
-            left: isSafari ? "calc(83.33vw - 7px)" : "calc(83.33vw - 10px)",
+            left: isSafari ? "calc(83.33vw - 13px)" : "calc(83.33vw - 10px)",
           }}
           whileInView={{
             height: "500px",
@@ -93,7 +93,7 @@ const Experience = ({ data }) => {
         <motion.div
           initial={{
             height: "0px",
-            left: isSafari ? "calc(50vw)" : "calc(50vw - 2px)",
+            left: isSafari ? "calc(50vw - 5px)" : "calc(50vw - 2px)",
           }}
           whileInView={{
             height: "300px",
