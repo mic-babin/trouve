f573a4d413d25e7eb2888cb78a8356a55b1dd741
Michael Babin <mic.babin@outlook.com>
2023-11-12 07:18:16 -0500

    test rebuild with no script

--- src/pages/merci.js
+++ src/pages/merci.js
@@ -52,7 +52,13 @@ export const Head = () => (
     <!-- Google tag (gtag.js) --> <script async src="https://www.googletagmanager.com/gtag/js?id=G-MFY0HQY3X7"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-MFY0HQY3X7'); </script>`}
     </script>
     <script>{`gtag('event', 'conversion', {'send_to': 'AW-11372992172/I6K8CIXF6usYEKytiK8q'});`}</script>
-
+    <script>
+      {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-5VKFN36S');`}
+    </script>
     <SEO title="Merci - " />
   </>
 );
